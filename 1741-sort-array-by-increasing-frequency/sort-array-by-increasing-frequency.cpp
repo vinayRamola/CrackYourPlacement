89799468b7f79b0cdc1d87ae5da6d1b8b98051d2
vinayRamola <117000775+vinayRamola@users.noreply.github.com>
2024-07-23 07:46:24 +0530

    Time: 5 ms (68.31%) | Memory: 15 MB (25.59%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool static comp(pair<int,int> p1,pair<int,int> p2){
+    static bool comp(pair<int,int> p1,pair<int,int> p2){
         if(p1.second<p2.second) return true;
         else if(p1.second==p2.second) return p1.first>p2.first;
         else return false;
