96735e68b946fb413ae5d829a799e2df844fbc17
vinayRamola <117000775+vinayRamola@users.noreply.github.com>
2024-07-23 07:42:23 +0530

    Time: 10 ms (29.58%) | Memory: 14.9 MB (55.33%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool static comp(pair<int,int> p1,pair<int,int> p2){
+        if(p1.second<p2.second) return true;
+        else if(p1.second==p2.second) return p1.first>p2.first;
+        else return false;
+    }
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        for(auto num:nums){
+            mp[num]++;
+        }
+        vector<pair<int,int>> v(mp.begin(),mp.end());
+        sort(v.begin(),v.end(),comp);
+
+        vector<int> ans;
+
+        for(auto ele:v){
+            while(ele.second--)
+                ans.push_back(ele.first);
+        }
+        return ans;
+    }
+};
