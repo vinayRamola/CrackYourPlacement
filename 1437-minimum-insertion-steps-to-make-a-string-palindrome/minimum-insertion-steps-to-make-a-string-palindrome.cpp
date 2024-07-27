62d6cfeda8604d79e0218cffb17595b0a6e95d7d
vinayRamola <117000775+vinayRamola@users.noreply.github.com>
2024-07-27 22:02:05 +0530

    Time: 16 ms (95.09%) | Memory: 29.9 MB (23.08%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -13,9 +13,25 @@ public:
     }
     int minInsertions(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
+        // vector<vector<int>> dp(n,vector<int> (n,-1));
         
-        return  solve(s,0,n-1,dp);
+        // return  solve(s,0,n-1,dp);
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // Fill the DP table
+        for (int length = 2; length <= n; ++length) {
+            for (int i = 0; i <= n - length; ++i) {
+                int j = i + length - 1;
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
         
     }
 };
