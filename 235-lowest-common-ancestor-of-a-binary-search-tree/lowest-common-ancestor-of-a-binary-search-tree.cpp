4d5bb80c0f03d15e547f7d62d2390af550b05c2b
vinayRamola <117000775+vinayRamola@users.noreply.github.com>
2024-07-29 22:35:30 +0530

    Time: 26 ms (33.00%) | Memory: 22.2 MB (7.29%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // if(root==NULL){
+        //     return root;
+        // }
+        // if(root->val == p->val || root->val == q->val){
+        //     return root;
+        // }
+
+        // TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+        // TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
+
+        // if(leftAns != NULL and rightAns != NULL){
+        //     return root;
+        // }
+        // else if(leftAns != NULL and rightAns == NULL){
+        //     return leftAns;
+        // }
+        // else if(leftAns == NULL and rightAns != NULL){
+        //     return rightAns;
+        // }
+
+        // else{
+        //     return NULL;
+        // }
+
+        if(root==NULL) return NULL;
+
+        if(root == p || root==q) return root;
+
+        TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
+
+        if(leftAns !=NULL and rightAns ==NULL) return leftAns;
+        else if(leftAns ==NULL and rightAns !=NULL) return rightAns;
+        else if(leftAns != NULL and rightAns != NULL) return root;
+        else return NULL;
+    }
+};
