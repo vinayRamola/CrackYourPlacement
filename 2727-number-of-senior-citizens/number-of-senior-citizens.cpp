3a06d5890db023e877a6a37f803b7dd837a566c2
vinayRamola <117000775+vinayRamola@users.noreply.github.com>
2024-08-01 07:49:48 +0530

    Time: 13 ms (5.72%) | Memory: 17.7 MB (16.71%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int n = details.size();
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            int age = stoi(details[i].substr(11,2));
+            if(age > 60) count++;
+        }
+        return count;
+    }
+};
