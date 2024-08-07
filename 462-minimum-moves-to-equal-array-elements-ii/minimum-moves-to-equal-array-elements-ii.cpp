0a2c8ffcdfdd6b13aca0a87f13d034ba144c2c68
vinayRamola <117000775+vinayRamola@users.noreply.github.com>
2024-08-07 11:21:49 +0530

    Time: 10 ms (42.07%) | Memory: 14.9 MB (11.98%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,17 +4,11 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
 
-        if(n%2==0) return (n/2);
-        else return (n/2);
+        return n/2;
     }
     int minMoves2(vector<int>& nums) {
         int n = nums.size();
         int sum=0;
-
-        // if(n%2==0) center = median(nums)
-        // for(auto &num:nums) sum+=num;
-
-        // int avg = sum/n;
 
         int center = median(nums);
 
